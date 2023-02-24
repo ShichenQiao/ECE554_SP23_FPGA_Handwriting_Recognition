f354571ed51ab8e667b3238fe9f0453fadb8c3ee
Haining Qiu <hqiu37@wisc.edu>
2023-02-24 17:06:51 -0600

    Update BMP.asm

--- BMP_display/BMP.asm
+++ BMP_display/BMP.asm
@@ -22,32 +22,121 @@ LHB R2, 0xC0
 
 ##### INITIALS #####
 # loc = 0,0
-LLB R3, 0x00
-SW	R3, R2, 1
-SW	R3, R2, 2
-
+SW	R0, R2, 1
+SW	R0, R2, 2
 # J
-LLB R3, 0x00
-LHB R3, 0x2A
+LLB R3, 0x80
+LHB R3, 0x29
 SW	R3, R2, 0	# write character
-LLB R4, 0x00
-LHB	R4, 0x10	# load R4 for waiting
 JAL	wait
 
-# loc = 25,0
-LLB	R3, 0x19
+# loc = 20,0
+LLB	R3, 0x14
 SW	R3, R2, 1
-LLB R3, 0x00
-SW	R3,	R2, 2
-
+SW	R0,	R2, 2
 # Q
 LLB R3,	0x00
-LHB	R3, 0x32
+LHB	R3, 0x2D
 SW	R3, R2, 0
-HLT
+JAL	wait
+
+# loc = 60,0
+LLB	R3, 0x3C
+SW	R3, R2, 1
+SW	R0,	R2, 2
+# H
+LLB	R3, 0x80
+LHB	R3, 0x28
+SW	R3, R2, 0
+JAL wait
+
+# loc = 80,0
+LLB	R3, 0x50
+SW	R3, R2, 1
+SW	R0,	R2, 2
+# Z
+LLB	R3, 0x80
+LHB R3, 0x31
+SW	R3,	R2, 0
+JAL wait
+
+# loc = 120,0
+LLB	R3, 0x78
+SW	R3, R2, 1
+SW	R0,	R2, 2
+# H
+LLB	R3, 0x80
+LHB	R3, 0x28
+SW	R3, R2, 0
+JAL wait
+
+# loc = 160,0
+LLB	R3, 0xA0
+SW	R3, R2, 1
+SW	R0,	R2, 2
+# Q
+LLB R3,	0x00
+LHB	R3, 0x2D
+SW	R3, R2, 0
+JAL	wait
+
+# loc = 180,0
+LLB	R3, 0xB4
+SW	R3, R2, 1
+SW	R0,	R2, 2
+# Q
+LLB R3,	0x00
+LHB	R3, 0x2D
+SW	R3, R2, 0
+JAL	wait
+
+# loc = 200,0
+LLB	R3, 0xC8
+SW	R3, R2, 1
+SW	R0,	R2, 2
+# L
+LLB	R3, 0x80
+LHB	R3, 0x2A
+SW	R3, R2, 0
+JAL wait
+
+# loc = 0,50
+SW	R0, R2, 1
+LLB	R3, 0x32
+SW	R3, R2, 2
+# image 0
+LLB	R3, 0x40
+SW	R3, R2, 0
+JAL	wait
+
+# loc = 200,50
+LLB	R3, 0xC8
+SW	R3, R2, 1
+LLB	R3, 0x32
+SW	R3, R2, 2
+# image 1
+LLB	R3, 0x41
+SW	R3, R2, 0
+JAL	wait
+
+# loc = 400,50
+LLB	R3, 0x90
+LHB	R3, 0x01
+SW	R3, R2, 1
+LLB	R3, 0x32
+SW	R3, R2, 2
+# image 2
+LLB	R3, 0x42
+SW	R3, R2, 0
+JAL	wait
+
+
 
 # waiting routine
 wait:
+LLB R4, 0x00
+LHB	R4, 0x10	# load R4 for waiting
+loop:
 SUB	R4, R4, R1	# R4 <- R4 - 1
-B	neq, wait
+B	neq, loop
 JR	R15
