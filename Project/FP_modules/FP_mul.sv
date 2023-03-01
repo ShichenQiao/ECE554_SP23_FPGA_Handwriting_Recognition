8d729f44b14c054d14d9453dd200aaf9d9dfd32f
Justin Qiao <71228724+ShichenQiao@users.noreply.github.com>
2023-03-01 13:49:43 -0600

    Removed unused code in fmul

--- Project/FP_modules/FP_mul.sv
+++ Project/FP_modules/FP_mul.sv
@@ -3,7 +3,8 @@ module FP_mul(A, B, OUT);
 	// SEEE EEEE EMMM MMMM MMMM MMMM MMMM MMMM
 	// value = (-1)^S * 2^(E-127) * (1.M)
 	// when |E = 0, value = (-1)^S * 2^(E-127) * (0.M)
-	// +0 = 32'h00000000, -0 = 32'h80000000
+	// +0 = 32'h0000_0000, -0 = 32'h8000_0000
+	// +INF = 32'h7F80_0000, -INF = 32'hFF80_0000
 
 	input [31:0] A;			// 16 bit input
 	input [31:0] B;			// 4 bit input
@@ -38,45 +39,5 @@ module FP_mul(A, B, OUT);
 	assign MO = prod_M[47] ? prod_M[47:24] : prod_M[46:23];
 
 	assign OUT = {SO, EO, MO[22:0]};
-
-/*
-	always begin
-		casex(prod_M)
-			32'b1xxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h00;
-			32'b01xx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h01;
-			32'b001x_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h02;
-			32'b0001_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h03;
-			32'b0000_1xxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h04;
-			32'b0000_01xx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h05;
-			32'b0000_001x_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h06;
-			32'b0000_0001_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h07;
-			32'b0000_0000_1xxx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h08;
-			32'b0000_0000_01xx_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h09;
-			32'b0000_0000_001x_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h0A;
-			32'b0000_0000_0001_xxxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h0B;
-			32'b0000_0000_0000_1xxx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h0C;
-			32'b0000_0000_0000_01xx_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h0D;
-			32'b0000_0000_0000_001x_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h0E;
-			32'b0000_0000_0000_0001_xxxx_xxxx_xxxx_xxxx: shift_amount = 6'h0F;
-			32'b0000_0000_0000_0000_1xxx_xxxx_xxxx_xxxx: shift_amount = 6'h10;
-			32'b0000_0000_0000_0000_01xx_xxxx_xxxx_xxxx: shift_amount = 6'h11;
-			32'b0000_0000_0000_0000_001x_xxxx_xxxx_xxxx: shift_amount = 6'h12;
-			32'b0000_0000_0000_0000_0001_xxxx_xxxx_xxxx: shift_amount = 6'h13;
-			32'b0000_0000_0000_0000_0000_1xxx_xxxx_xxxx: shift_amount = 6'h14;
-			32'b0000_0000_0000_0000_0000_01xx_xxxx_xxxx: shift_amount = 6'h15;
-			32'b0000_0000_0000_0000_0000_001x_xxxx_xxxx: shift_amount = 6'h16;
-			32'b0000_0000_0000_0000_0000_0001_xxxx_xxxx: shift_amount = 6'h17;
-			32'b0000_0000_0000_0000_0000_0000_1xxx_xxxx: shift_amount = 6'h18;
-			32'b0000_0000_0000_0000_0000_0000_01xx_xxxx: shift_amount = 6'h19;
-			32'b0000_0000_0000_0000_0000_0000_001x_xxxx: shift_amount = 6'h1A;
-			32'b0000_0000_0000_0000_0000_0000_0001_xxxx: shift_amount = 6'h1B;
-			32'b0000_0000_0000_0000_0000_0000_0000_1xxx: shift_amount = 6'h1C;
-			32'b0000_0000_0000_0000_0000_0000_0000_01xx: shift_amount = 6'h1D;
-			32'b0000_0000_0000_0000_0000_0000_0000_001x: shift_amount = 6'h1E;
-			32'b0000_0000_0000_0000_0000_0000_0000_0001: shift_amount = 6'h1F;
-			default:	shift_amount = 6'h20;		// should never happen
-		endcase
-	end
-*/
 
 endmodule
