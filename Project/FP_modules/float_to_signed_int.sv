dbadfb85c604255a60d3340ab660e7a54f47c23b
Justin Qiao <71228724+ShichenQiao@users.noreply.github.com>
2023-03-08 11:22:54 -0600

    added overflow/underflow protection to FTI

--- Project/FP_modules/float_to_signed_int.sv
+++ Project/FP_modules/float_to_signed_int.sv
@@ -11,10 +11,12 @@ module float_to_signed_int(FP_val, signed_int_val);
 	assign M = FP_val[22:0];
 	assign E = FP_val[30:23];
 
-    // calculate the absolute value of the integer
-    assign abs_int = {8'h00, |E, M} << (E - 8'd127);
+    // calculate the absolute value of the integer, choosing correct shift direction according to E
+    assign abs_int = E >= 8'd150 ? {8'h00, 1'b1, M} << (E - 8'd150) : {8'h00, 1'b1, M} >> (8'd150 - E);
 
-    // set the sign of the integer based on the sign bit of the floating-point value
-    assign signed_int_val = (FP_val[31]) ? {1'b1, (~abs_int + 31'd00000001)} : {1'b0, abs_int};
+    // convert abs_int to signed_int_val with saturation/zeroing logic
+    assign signed_int_val = E >= 8'd182 ? ((FP_val[31]) ? 32'h80000000 : 32'h7FFFFFFF) :			// if E too large, saturate
+							E < 8'd127 ? 32'h00000000 :												// if E too small, zero
+							(FP_val[31]) ? {1'b1, (~abs_int + 31'd00000001)} : {1'b0, abs_int};		// if neither of the extremes, output according to sign
 
 endmodule
--- Project/FP_modules/float_to_signed_int_tb.sv
+++ Project/FP_modules/float_to_signed_int_tb.sv
@@ -6,7 +6,7 @@ module float_to_signed_int_tb();
     logic signed [31:0] signed_int_val;			// output is 32 bit signed int
 
 	int exp_val;
-	int precision;
+	int precision_right_shift;
 
 	float_to_signed_int iDUT(
 		.FP_val(FP_val),
@@ -16,26 +16,43 @@ module float_to_signed_int_tb();
 	task automatic test_float_to_signed_int(shortreal test_val);
 		FP_val = $shortrealtobits(test_val);
 		exp_val = $rtoi(test_val);
-		precision = int'(FP_val[30:23] - 8'd127);
+		precision_right_shift = FP_val[30:23] <= 8'd150 ? 0 :
+					FP_val[30:23] < 8'd181 ? int'(FP_val[30:23] - 8'd150) :
+					0;
 		#1;
+		// if FP_val's abs value is too large, PASS if saturated
+		if((FP_val[30:23] >= 8'd182) && (signed_int_val[31] ? signed_int_val === 32'h80000000 : signed_int_val === 32'h7FFFFFFF) ) begin
+			return;
+		end
+
+		// if FP_val's abs value is between zero and one, PASS if zeroed
+		if((FP_val[30:23] < 8'd127) && ~|signed_int_val) begin
+			return;
+		end
+
 		// allow -1 ~ +1 difference on the scale of valid precision due to shortrealtobits and rtoi error
 		// if E in FP input is too large or too small, this if check will automatically let it PASS, since the converted int is meaningless anyways
-		if((signed_int_val >>> precision) > (exp_val >>> precision) + 1 ||
-		   (signed_int_val >>> precision) < (exp_val >>> precision) - 1) begin
-			$display("WRONG ANSWER! %b  %b", signed_int_val >>> precision, exp_val >>> precision);
+		if((signed_int_val >>> precision_right_shift) > (exp_val >>> precision_right_shift) + 1 ||
+		   (signed_int_val >>> precision_right_shift) < (exp_val >>> precision_right_shift) - 1) begin
+			$display("WRONG ANSWER! %b  %b", signed_int_val >>> precision_right_shift, exp_val >>> precision_right_shift);
 			$stop();
 		end
 	endtask
 
 	initial begin
 		// random tests
-		for(int i = 0; i < 100; i++) begin
-			test_float_to_signed_int($random());
+		for(int i = 0; i < 1000; i++) begin
+			test_float_to_signed_int($random());			// intentionally not casting to use the 32 random bits as shortreal
+		end
+
+		// random tests of small integers
+		for(int i = 0; i < 1000; i++) begin
+			test_float_to_signed_int($itor($random() % 100000));
 		end
 
 		// special FP value tests
 		for(int i = 0; i < 16; i++) begin
-			test_float_to_signed_int(SPECIAL_VALS_ARR[i]);
+			test_float_to_signed_int($bitstoshortreal(SPECIAL_VALS_ARR[i]));
 		end
 
 		$display("ALL TESTS PASSED!!!");
