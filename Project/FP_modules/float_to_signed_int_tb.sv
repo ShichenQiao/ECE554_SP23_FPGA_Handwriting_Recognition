328edbc3524797c779bff721a327b81ccd82622a
Justin Qiao <71228724+ShichenQiao@users.noreply.github.com>
2023-03-07 18:01:07 -0600

    Added FP special value tests

--- Project/FP_modules/float_to_signed_int_tb.sv
+++ Project/FP_modules/float_to_signed_int_tb.sv
@@ -1,5 +1,7 @@
 module float_to_signed_int_tb();
 
+	import FP_special_values::*;
+
     logic [31:0] FP_val;						// input is 32 bit float
     logic signed [31:0] signed_int_val;			// output is 32 bit signed int
 
@@ -16,6 +18,8 @@ module float_to_signed_int_tb();
 		exp_val = $rtoi(test_val);
 		precision = int'(FP_val[30:23] - 8'd127);
 		#1;
+		// allow -1 ~ +1 difference on the scale of valid precision due to shortrealtobits and rtoi error
+		// if E in FP input is too large or too small, this if check will automatically let it PASS, since the converted int is meaningless anyways
 		if((signed_int_val >>> precision) > (exp_val >>> precision) + 1 ||
 		   (signed_int_val >>> precision) < (exp_val >>> precision) - 1) begin
 			$display("WRONG ANSWER! %b  %b", signed_int_val >>> precision, exp_val >>> precision);
@@ -27,6 +31,11 @@ module float_to_signed_int_tb();
 		// random tests
 		for(int i = 0; i < 100; i++) begin
 			test_float_to_signed_int($random());
+		end
+
+		// special FP value tests
+		for(int i = 0; i < 16; i++) begin
+			test_float_to_signed_int(SPECIAL_VALS_ARR[i]);
 		end
 
 		$display("ALL TESTS PASSED!!!");
