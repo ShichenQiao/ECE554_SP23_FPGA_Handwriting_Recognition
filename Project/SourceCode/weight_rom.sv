7f16871c404040261ad854a39f1b1430355568e5
lzhao224 <hellozhshk@outlook.com>
2023-03-09 17:25:23 -0600

    add top level modules

--- Project/SourceCode/weight_rom.sv
+++ Project/SourceCode/weight_rom.sv
@@ -4,10 +4,13 @@ module weight_rom(clk,raddr,rdata);
   input [12:0] raddr;
   output reg [31:0] rdata;
   
-  reg [31:0]mem[0:7830];   //The current weight mem is 32 bit floating point x (28x28) x 10
+  reg [31:0]rom[0:7830];   //The current weight mem is 32 bit floating point x (28x28) x 10
+
+  initial
+    $readmemh("../weight.hex",rom);
 
   always @(posedge clk) begin
-    rdata <= mem[raddr];
+    rdata <= rom[raddr];
   end
 
   initial begin
