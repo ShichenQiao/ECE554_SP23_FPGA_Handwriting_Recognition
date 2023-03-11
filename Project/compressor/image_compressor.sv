65ebe6c427aa8252fe080fa2c84023a48882cc49
Haining Qiu <hqiu37@wisc.edu>
2023-03-11 16:51:01 -0600

    Not verified

--- Project/compressor/image_compressor.sv
+++ Project/compressor/image_compressor.sv
@@ -24,32 +24,48 @@ output sram_wr;					// SRAM write enable
 output [7:0] pix_color_out;		// 8-bit pixel color output
 								// that's averaged from 8*8 block
 								
-output [9:0] compress_addr;		// pixel address after compression
-								// ranging from 0 to 783 (28*28)
-
+output logic [9:0] compress_addr;	// pixel address after compression
+									// ranging from 0 to 783 (28*28)
+								
+logic clr_block;				// set when current block should be cleared
+logic [13:0] accum;				// accumulated sum of a block
 logic [13:0] block [0:27];		// 28 14-bit registers which stores
 								// the sum of color bits of a block
 								
 logic [4:0] b_haddr;			// block column address
-logic [4:0] b_vaddr;            // block row address
 logic we;						// write enable of block regsiters
-logic [13:0] block_out;			// block regsiter output
+logic [13:0] block_out;			// accumulated block output
+
+// compressor address ff & SRAM write enable
+assign sram_wr = (&pix_haddr[2:0]) & // enable a write when an end-of-block
+				 (&pix_vaddr[2:0]) & // occurs, but not when 0x3FF,0x3FF
+				 we &	 			 // or when address exceeds image size
+				 ~(compress_addr == 10'd784);
+
+always @(posedge clk, negedge rst_n)
+	if (!rst_n)
+		compress_addr <= 10'd784;
+	else if (start)		// start from 0
+		compress_addr <= 10'h000;
+	else if (sram_wr)	// increment when current block written to SRAM
+		compress_addr <= compress_addr + 10'b1;
 
 // determine b_haddr and b_vaddr using pix_haddr and pix_vaddr
 assign b_haddr = pix_haddr[7:3];
-assign b_vaddr = pix_vaddr[7:3];
-
-// determine compress_addr using b_haddr and b_vaddr
-assign compress_addr = {5'b0,b_vaddr} * 10'd28 + {5'b0,b_haddr};
 
 // block registers and its write enable signal
 // when any pix address is 0xFF, disable write
 assign we = ~&pix_haddr & ~&pix_vaddr;
-assign block_out = block[b_haddr];
+// clear block when both pixel addresses are multiples of 8
+assign clr_block = ~|pix_haddr[2:0] & ~|pix_vaddr[2:0];
+assign block_out = clr_block ? 14'b0 : block[b_haddr];
+assign accum = {6'b0,pix_color_in} + block_out;
+// block ff
 always @(posedge clk)
 	if (we)
-		block[b_haddr] <= {6'b0,pix_color_in} + block_out;
+		block[b_haddr] <= accum;
 		
-
+// compressed average output
+assign pix_color_out = accum[13:6];
 
 endmodule
