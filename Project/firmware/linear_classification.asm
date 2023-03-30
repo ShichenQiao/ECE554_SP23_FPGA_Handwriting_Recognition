e69d81fc11c8c728a4f97e9cb70c11517263668b
Justin Qiao <71228724+ShichenQiao@users.noreply.github.com>
2023-03-30 17:59:23 -0500

    finalized linear_classification.asm

--- Project/firmware/linear_classification.asm
+++ Project/firmware/linear_classification.asm
@@ -7,9 +7,9 @@
 # 	R4 - matrix size
 # 	R5 - matrix index
 # 	R6 - Snapshot status
-# 	R28 - 0x0000C000 base address of peripherals
-# 	R29 - reserved for result of matrix multiplication
-# 	R30 - score pointer, scores will be in DM at addr = 1000 through 1009
+# 	R28 - reserved for result of matrix multiplication
+# 	R29 - score pointer, scores will be in DM at addr = 1000 through 1009
+# 	R30 - 0x0000C000 base address of peripherals
 #	R31 - reserved for JAL/JR
 #
 ###########################################################
@@ -21,15 +21,15 @@
 # Load R1 with 1
 LLB		R1, 1
 
-# load R28 with 0x0000C000
-LLB		R28, 0xC000
-LHB		R28, 0x0000
+# load R30 with 0x0000C000
+LLB		R30, 0xC000
+LHB		R30, 0x0000
 
 CLASSIFY:
-SW		R1, R28, 8					# send one snapshot request
+SW		R1, R30, 8					# send one snapshot request
 SNAPSHOT_WAIT:
-LW		R6, R28, 8					# get the snapshot request status
-SUB		R6, R6, R1				# check if it is one
+LW		R6, R30, 8					# get the snapshot request status
+SUB		R6, R6, R1					# check if it is one
 B		NEQ, SNAPSHOT_WAIT			# if the status is still 1(meaning waiting for snapshot), then keep waiting
 
 ##########################
@@ -50,8 +50,8 @@ LLB		R4, 784
 # Load R5 with 10
 LLB		R5, 10
 
-# Load R30 with 1000
-LLB		R30, 1000
+# Load R29 with 1000
+LLB		R29, 1000
 
 #########################
 # MATRIX MULTIPLY STAGE #
@@ -60,16 +60,16 @@ LLB		R30, 1000
 MATRIX_LOOP:
 # Call matrix multiplication and store result to DM
 JAL		MATRIX_MUL
-SW		R29, R30, 0
-ADD		R30, R30, R1
+SW		R28, R29, 0
+ADD		R29, R29, R1
 ADD		R2, R2, R4
 
 # loop back when not finished
 SUB		R5, R5, R1
 B		NEQ, MATRIX_LOOP
 
-# Load R30 with 1000 for the output stage
-LLB		R30, 1000
+# Load R29 with 1000 for the output stage
+LLB		R29, 1000
 
 ##############################
 # CLASIFICATION OUTPUT STAGE #
@@ -92,7 +92,7 @@ B		UNCOND, CLASSIFY
 #	R4 - matrix size
 #
 #	Return:
-#	R29 - result of matrix calculation	
+#	R28 - result of matrix calculation	
 #
 #	Reg Usage:
 #	R0 - hard-wired 0
@@ -101,6 +101,7 @@ B		UNCOND, CLASSIFY
 #	R6 - image pixel value
 #	R7 - weight value
 #	R8 - multiplication result
+#	R31 - reserved for JAL/JR
 #
 ###########################################################
 MATRIX_MUL:
@@ -138,15 +139,15 @@ B		NEQ, MUL_LOOP
 
 # R5 <- 0x00000000
 ADD		R5, R0, R0
-# R29 <- 0x00000000
-ADD		R29, R0, R0
+# R28 <- 0x00000000
+ADD		R28, R0, R0
 # restore R4
 POP		R4
 
 # additions
 SEQ_ADD:
 LW		R8, R5, 0
-ADDF	R29, R29, R8
+ADDF	R28, R28, R8
 
 # increment pointer
 ADD		R5, R5, R1
@@ -173,7 +174,7 @@ JR		R31
 #	A function call to find max score in the output layer
 #
 #	Params:
-#	R30 - base pointer to the output layer
+#	R29 - base pointer to the output layer
 #
 #	Return:
 #	None
@@ -181,6 +182,16 @@ JR		R31
 #	Reg Usage:
 #	R0 - hard-wired 0
 #	R1 - 1
+#	R2 - loop index i
+#	R3 - loop terminate condition = 9
+#	R4 - current number
+#	R5 - current number - current max
+#	R6 - 9 - i
+#	R7 - current max
+#	R8 - current max index
+#	R9 - 0x00000030 ASCII number offset
+# 	R30 - 0x0000C000 base address of peripherals
+#	R31 - reserved for JAL/JR
 #
 ###########################################################
 OUTPUT_LAYER:
@@ -193,8 +204,7 @@ PUSH	R6
 PUSH	R7
 PUSH	R8
 PUSH	R9
-PUSH	R28
-PUSH	R30
+PUSH	R29
 
 # load 0x00000030 into R9
 LLB		R9, 0x0030
@@ -212,11 +222,11 @@ LLB		R3, 0x0009
 LOAD_NEXT:
 SUB		R6, R3, R2
 B		LT, DONE
-LW		R4, R30, 0
-SUBF	R5, R4, R7			# result in POS_INF at the first time
+LW		R4, R29, 0
+SUBF	R5, R4, R7				# result in POS_INF at the first time
 B		GT, NEW_MAX
 ADD		R2, R2, R1				# increment loop index
-ADD		R30, R30, R1			# increment address
+ADD		R29, R29, R1			# increment address
 B		UNCOND, LOAD_NEXT
 
 # when current number > current max
@@ -226,17 +236,16 @@ NEW_MAX:
 ADD		R7, R4, R0
 ADD		R8, R2, R0
 ADD		R2, R2, R1				# increment loop index
-ADD		R30, R30, R1			# increment address
+ADD		R29, R29, R1			# increment address
 B		UNCOND, LOAD_NEXT
 
 # max found, print to SPART
 DONE:
-ADD		R8, R8, R9			# R8 <- R8 + 0x0030
-SW		R8, R28, 4				# print to SPART
+ADD		R8, R8, R9				# R8 <- R8 + 0x0030
+SW		R8, R30, 4				# print to SPART
 
 # restore saved registers
-POP		R30
-POP		R28
+POP		R29
 POP		R9
 POP		R8
 POP		R7
